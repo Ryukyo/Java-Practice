5bea205035bfd8d514a3c472d3641e829aee14c6
Florian Doitsukara <florian.fejer@hotmail.de>
2019-09-09 21:19:23 +0200

    Added StockList and StockItem and cleaned up Main after testing

--- SortedCollections/src/com/flo/Main.java
+++ SortedCollections/src/com/flo/Main.java
@@ -44,50 +44,91 @@ public class Main {
             System.out.println(s);
         }
 
-        Basket timsBasket = new Basket("Tim");
-        sellItem(timsBasket, "car", 1);
-        System.out.println(timsBasket);
+        Basket myBasket = new Basket("Flo");
 
-        sellItem(timsBasket, "car", 1);
-        System.out.println(timsBasket);
+        sellItem(myBasket, "car", 1);
+        System.out.println(myBasket);
 
-        if(sellItem(timsBasket, "car", 1) != 1) {
+        sellItem(myBasket, "car", 1);
+        System.out.println(myBasket);
+
+        if(sellItem(myBasket, "car", 1) != 1) {
             System.out.println("There are no more cars in stock");
         }
 
-        sellItem(timsBasket, "spanner", 5);
-        System.out.println(timsBasket);
+        sellItem(myBasket, "spanner", 5);
 
-        sellItem(timsBasket, "juice", 4);
-        sellItem(timsBasket, "cup", 12);
-        sellItem(timsBasket, "bread", 1);
-        System.out.println(timsBasket);
+        sellItem(myBasket, "juice", 4);
+        sellItem(myBasket, "cup", 12);
+        sellItem(myBasket, "bread", 1);
 
+        Basket basket = new Basket("customer");
+        sellItem(basket, "cup", 100);
+        sellItem(basket, "juice", 5);
+        removeItem(basket, "cup", 1);
+        System.out.println(basket);
+
+        removeItem(myBasket, "car", 1);
+        removeItem(myBasket, "cup", 9);
+        removeItem(myBasket, "car", 1);
+        System.out.println("cars removed: " + removeItem(myBasket, "car", 1));  // should not remove any
+
+        System.out.println(myBasket);
+
+        removeItem(myBasket, "bread", 1);
+        removeItem(myBasket, "cup", 3);
+        removeItem(myBasket, "juice", 4);
+        removeItem(myBasket, "cup", 3);
+        System.out.println(myBasket);
+
+        System.out.println("\nDisplay stock list before and after checkout");
+        System.out.println(basket);
+        System.out.println(stockList);
+        checkOut(basket);
+        System.out.println(basket);
         System.out.println(stockList);
 
-//        temp = new StockItem("pen", 1.12);
-//        stockList.Items().put(temp.getName(), temp);
-        stockList.Items().get("car").adjustStock(2000);
-        stockList.Items().get("car").adjustStock(-1000);
-        System.out.println(stockList);
-        for(Map.Entry<String, Double> price: stockList.PriceList().entrySet()) {
-            System.out.println(price.getKey() + " costs " + price.getValue());
+        StockItem car = stockList.Items().get("car");
+        if(car != null) {
+            car.adjustStock(2000);
+        }
+        if(car != null) {
+            stockList.get("car").adjustStock(-1000);
         }
 
-
+        System.out.println(stockList);
+        checkOut(myBasket);
+        System.out.println(myBasket);
     }
 
     public static int sellItem(Basket basket, String item, int quantity) {
-        // retrieve the item from stock list
         StockItem stockItem = stockList.get(item);
         if(stockItem == null) {
             System.out.println("We don't sell " + item);
             return 0;
         }
-        if(stockList.sellStock(item, quantity) != 0) {
-            basket.addToBasket(stockItem, quantity);
-            return quantity;
+        if(stockList.reserveStock(item, quantity) != 0) {
+            return basket.addToBasket(stockItem, quantity);
         }
         return 0;
+    }
+
+    public static int removeItem(Basket basket, String item, int quantity) {
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(basket.removeFromBasket(stockItem, quantity) == quantity) {
+            return stockList.unreserveStock(item, quantity);
+        }
+        return 0;
+    }
+
+    public static void checkOut(Basket basket) {
+        for (Map.Entry<StockItem, Integer> item : basket.Items().entrySet()) {
+            stockList.sellStock(item.getKey().getName(), item.getValue());
+        }
+        basket.clearBasket();
     }
 }
--- SortedCollections/src/com/flo/StockList.java
+++ SortedCollections/src/com/flo/StockList.java
@@ -4,9 +4,6 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
- * Created by dev on 16/02/2016.
- */
 public class StockList {
     private final Map<String, StockItem> list;
 
@@ -16,13 +13,10 @@ public class StockList {
 
     public int addStock(StockItem item) {
         if(item != null) {
-            // check if already have quantities of this item
             StockItem inStock = list.getOrDefault(item.getName(), item);
-            // If there are already stocks on this item, adjust the quantity
             if(inStock != item) {
                 item.adjustStock(inStock.availableQuantity());
             }
-
             list.put(item.getName(), item);
             return item.availableQuantity();
         }
@@ -30,11 +24,26 @@ public class StockList {
     }
 
     public int sellStock(String item, int quantity) {
-        StockItem inStock = list.getOrDefault(item, null);
+        StockItem inStock = list.get(item);
 
-        if((inStock != null) && (inStock.availableQuantity() >= quantity) && (quantity >0)) {
-            inStock.adjustStock(-quantity);
-            return quantity;
+        if((inStock != null) && (quantity > 0)) {
+            return inStock.finaliseStock(quantity);
+        }
+        return 0;
+    }
+
+    public int reserveStock(String item, int quantity) {
+        StockItem inStock = list.get(item);
+        if((inStock != null) && (quantity > 0)) {
+            return inStock.reserveStock(quantity);
+        }
+        return 0;
+    }
+
+    public int unreserveStock(String item, int quantity) {
+        StockItem inStock = list.get(item);
+        if((inStock != null) && (quantity > 0)) {
+            return inStock.unreserveStock(quantity);
         }
         return 0;
     }
@@ -43,13 +52,13 @@ public class StockList {
         return list.get(key);
     }
 
-    public Map<String, Double> PriceList() {
-        Map<String, Double> prices = new LinkedHashMap<>();
-        for(Map.Entry<String, StockItem> item : list.entrySet()) {
-            prices.put(item.getKey(), item.getValue().getPrice());
-        }
-        return Collections.unmodifiableMap(prices);
-    }
+//    public Map<String, Double> PriceList() {
+//        Map<String, Double> prices = new LinkedHashMap<>();
+//        for(Map.Entry<String, StockItem> item : list.entrySet()) {
+//            prices.put(item.getKey(), item.getValue().getPrice());
+//        }
+//        return Collections.unmodifiableMap(prices);
+//    }
 
     public Map<String, StockItem> Items() {
         return Collections.unmodifiableMap(list);
@@ -61,14 +70,12 @@ public class StockList {
         double totalCost = 0.0;
         for (Map.Entry<String, StockItem> item : list.entrySet()) {
             StockItem stockItem = item.getValue();
-
             double itemValue = stockItem.getPrice() * stockItem.availableQuantity();
 
             s = s + stockItem + ". There are " + stockItem.availableQuantity() + " in stock. Value of items: ";
             s = s + String.format("%.2f",itemValue) + "\n";
             totalCost += itemValue;
         }
-
         return s + "Total stock value " + totalCost;
     }
 }
